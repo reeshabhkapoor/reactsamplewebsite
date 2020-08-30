87f66bb963162013c4b44a5a5ca7598607473a10
reeshabh.kapoor <reeshabh.kapoor@tripstack.com>
2020-08-31 02:04:36 +0530

    Updates

---
+++ precache-manifest.62b2185f466e260800731244382117b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409dc5373fe308c5e6cdafe174ab2283",
+    "url": "/reactsamplewebsite/index.html"
+  },
+  {
+    "revision": "408258986da5b02b309a",
+    "url": "/reactsamplewebsite/static/css/main.cfb5a67d.chunk.css"
+  },
+  {
+    "revision": "741e6ef6c84b06c7206b",
+    "url": "/reactsamplewebsite/static/js/2.af256c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactsamplewebsite/static/js/2.af256c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408258986da5b02b309a",
+    "url": "/reactsamplewebsite/static/js/main.452ca818.chunk.js"
+  },
+  {
+    "revision": "7d4c01bfed1e5eef27bd",
+    "url": "/reactsamplewebsite/static/js/runtime-main.3219df22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactsamplewebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
